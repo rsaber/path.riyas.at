678420d86f3f82569fe7f925ed5a6621c92dec56
Riyasat Saber <riyasatsaber@hotmail.com>
2018-09-08 18:39:55 +1000

    Add itin names

--- gensite.py
+++ gensite.py
@@ -5,7 +5,7 @@ import glob
 from collections import namedtuple
 import pytoml as toml
 
-from shutil import copytree
+from shutil import copytree, rmtree
     
 from jinja2 import Environment, FileSystemLoader
 env = Environment(loader=FileSystemLoader('templates'))
@@ -30,8 +30,9 @@ for itin_path in glob.glob( os.path.join(path, '*.toml') ):
 
 output_dir = '_site'
 
-if not os.path.exists(output_dir):
-    os.makedirs(output_dir)
+if os.path.exists(output_dir):
+    rmtree(output_dir)
+os.makedirs(output_dir)
 
 try:
     copytree(os.path.join('templates', 'css'), os.path.join(output_dir, 'css'))
@@ -39,21 +40,27 @@ try:
 except:
     pass
 
-nav = {
-    'Austria' : [
-        ('Vienna', True), 
-        ('Rome', False), 
-        ('Hamburg', False), 
-    ]
-}
+nav = {}
+
+# populate navbar (probably couldve been done in file search or site_structure)
 
 for country, itinaries in site_structure.items():
-    country_path = os.path.join(output_dir, country)
+    nav[country] = []
+    for itinerary in itinaries:
+        filename = '{}_{}.html'.format(country, itinerary['itinerary_name'])
+        output_path = os.path.join(output_dir, filename)
+        nav[country].append(
+            {
+                'itinerary_name': itinerary['itinerary_name'],
+                'link': filename
+            }
+        )
 
-    for plan in itinaries:
-        output_path = os.path.join(output_dir, '{}_{}.html'.format(country, plan['city']))
+for country, itinaries in site_structure.items():
+    for itinerary in itinaries:
+        output_path = os.path.join(output_dir, '{}_{}.html'.format(country, itinerary['itinerary_name']))
         template = env.get_template('template.html')
-        output = template.render(nav=nav, itin=plan)
+        output = template.render(nav=nav, itin=itinerary)
         with open(output_path, "w") as f:
             f.write(output)
         
