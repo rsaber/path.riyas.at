8ec6e58e10192b6415967ed3b443fdc7988b1b7a
Riyasat Saber <riyasatsaber@hotmail.com>
2018-09-08 16:44:38 +1000

    Read in tomls

--- gensite.py
+++ gensite.py
@@ -2,10 +2,31 @@
 
 import os
 import glob
-    
+from collections import namedtuple
+import pytoml as toml
+
+Itinerary = namedtuple('Itinerary', 'path country city')
+
 itins = []
 
+site_structure = {}
+
 path = 'itins/'
-for itin in glob.glob( os.path.join(path, '*.toml') ):
-    print "current file is: " + infile
-    itins.append(itin)
+for toml_itin in glob.glob( os.path.join(path, '*.toml') ):
+
+    cats = os.path.splitext(os.path.basename(toml_itin))[0].split('_')
+
+    itins.append(
+        Itinerary(
+            toml_itin,
+            os.path.splitext(os.path.basename(toml_itin))[0].split('_')[0],
+            os.path.splitext(os.path.basename(toml_itin))[0].split('_')[1]
+        )
+    )
+
+print(itins)
+
+for itin in itins:
+    with open(itin.path, "rb") as toml_file:
+        obj = toml.load(toml_file)
+    print(obj)
